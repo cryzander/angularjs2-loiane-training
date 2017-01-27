95d62afd301b2c719054c894eac9a29040d796cd
LUGO <luis.sales@iff.edu.br>
2017-01-27 13:16:30 -0200

    Comentarios 1

---
+++ _07-rotas/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { AuthGuard } from './guards/auth.guard';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+//import { CursosComponent } from './cursos/cursos.component';
+//import { routing } from './app.routing';
+//import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+//import { CursosService } from './cursos/cursos.service';
+//import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+import { AppRoutingModule } from './app.routing.module';
+//import { CursosModule } from './cursos/cursos.module';
+//import { AlunosModule } from './alunos/alunos.module';
+import { AuthService } from './login/auth.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent/*,
+    CursosComponent,
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent*/
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    //CursosModule,
+    //AlunosModule,
+    AppRoutingModule
+    //routing
+  ],
+  providers: [AuthService, AuthGuard],
+  //providers: [CursosService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ _07-rotas/src/app/app.routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+//import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+//import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+//import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+import { AuthGuard } from './guards/auth.guard';
+
+const appRoutes: Routes = [
+    { path: 'cursos', 
+        loadChildren: 'app/cursos/cursos.module#CursosModule',
+        canActivate: [AuthGuard]
+    },
+    { path: 'alunos',
+        loadChildren: 'app/alunos/alunos.module#AlunosModule',
+        canActivate: [AuthGuard]
+    },
+    //{ path: 'cursos', component: CursosComponent },
+    //{ path: 'curso/:id', component: CursoDetalheComponent },
+    { path: 'login', component: LoginComponent },
+    //{ path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
+    { path: '', component: HomeComponent,
+        canActivate: [AuthGuard] 
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
